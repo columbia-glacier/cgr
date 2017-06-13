e14bfbfc849f3e07f7be5c6b3505605dabf1f1d3
ezwelty <ethan.welty@gmail.com>
2017-06-13 17:58:11 +0200

    Add . to global variables

--- R/cgr.R
+++ R/cgr.R
@@ -4,3 +4,8 @@
 #' @name cgr
 #' @import magrittr
 NULL
+
+# Quiets concerns of R CMD CHECK: no visible binding for global variable '.'
+if (getRversion() >= "2.15.1") {
+  utils::globalVariables(c("."))
+}
