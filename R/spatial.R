a72d6752919424d0eade0cc049f6443863649a8e
ezwelty <ethan.welty@gmail.com>
2017-08-19 17:22:19 -0600

    Make sp_transform return same class as input

--- R/spatial.R
+++ R/spatial.R
@@ -6,9 +6,12 @@
 #' @param from Current proj.4 string.
 #' @param to Target proj.4 string.
 #' @param cols Column numbers or names specifying which \code{xy} columns are x and y coordinates.
+#' @return Object of the same class and dimensions as \code{xy} with coordinates transformed in place.
 #' @export
 #' @family spatial functions
 #' @examples
+#' xy <- c(-147.25079, 61.18586, 505)
+#' sp_transform(xy, "+proj=longlat +datum=WGS84", "+proj=utm +zone=6")
 #' xy <- c(x = -147.25079, y = 61.18586, z = 505)
 #' sp_transform(xy, "+proj=longlat +datum=WGS84", "+proj=utm +zone=6")
 #' xy <- data.frame(z = 505, x = -147.25079, y = 61.18586)
@@ -19,17 +22,23 @@ sp_transform <- function(xy, from, to, cols = 1:2) {
     gsub("+datum=NAD27", "+ellps=clrk66 +towgs84=-5,135,172 +units=m +no_defs", .)
   to %<>%
     gsub("+datum=NAD27", "+ellps=clrk66 +towgs84=-5,135,172 +units=m +no_defs", .)
-  # Coerce xy to data.frame
-  if (is.vector(xy)) {
-    xy %<>% t()
+  # Coerce to data.frame
+  is_vector <- is.vector(xy)
+  txy <- xy
+  if (is_vector) {
+    txy %<>%
+      t() %>%
+      as.data.frame()
   }
-  xy %<>% as.data.frame()
-  # Transform xy
-  txy <- xy %>%
+  # Transform coordinates
+  txy %<>%
     sp::`coordinates<-`(cols) %>%
     sp::`proj4string<-`(sp::CRS(from)) %>%
     sp::spTransform(sp::CRS(to))
-  # Build output
-  xy[, cols] <- txy@coords
+  if (is_vector) {
+    xy[cols] <- unlist(txy@coords)
+  } else {
+    xy[, cols] <-txy@coords
+  }
   xy
 }
